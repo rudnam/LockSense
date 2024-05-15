25504c2c9f76d7f1c1f5d10d722f69819903aef3
Francis A <francisalbarracin2@gmail.com>
2024-05-15 17:43:42 +0800

    feat: add push notifications (#17)
    * Set up push notifications
    
    * Modify CI to work for all branches

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:locksense/services/firebase_service.dart';
 import './main_screen.dart';
 import './theme/style.dart';
 import './firebase_options.dart';
@@ -7,6 +8,7 @@ import './firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseService().initNotifications();
   runApp(const MyApp());
 }
 
--- app/lib/services/firebase_service.dart
+++ app/lib/services/firebase_service.dart
@@ -1,30 +1,39 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+@pragma('vm:entry-point')
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
 
 class FirebaseService {
-  late DatabaseReference _dbRef;
+  final FirebaseDatabase _firebaseDatabase = FirebaseDatabase.instance;
   late DatabaseReference ledStateRef;
   late DatabaseReference photoresistorStateRef;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
-  void initialize() {
-    FirebaseDatabase database = FirebaseDatabase.instance;
-    _dbRef = FirebaseDatabase.instance.ref();
-    ledStateRef = database.ref('led/state');
-    photoresistorStateRef = database.ref('photoresistor/state');
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    final fCMToken = await _firebaseMessaging.getToken();
+    await _firebaseDatabase.ref().child("FCMToken").set(fCMToken);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 
   Future<void> writeData(String path, dynamic newData) async {
-    await _dbRef.child(path).set(newData);
+    await _firebaseDatabase.ref().child(path).set(newData);
   }
 
   void addLedStateListener(void Function(String) listener) {
-    ledStateRef.onValue.listen((event) {
+    _firebaseDatabase.ref('led/state').onValue.listen((event) {
       final ledState = event.snapshot.value.toString();
       listener(ledState);
     });
   }
 
   void addPhotoresistorStateListener(void Function(String) listener) {
-    photoresistorStateRef.onValue.listen((event) {
+    _firebaseDatabase.ref('photoresistor/state').onValue.listen((event) {
       final photoresistorState = event.snapshot.value.toString();
       listener(photoresistorState);
     });
